d29e74e600c3b6f99ac49188e6e29740fbaf4da4
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-06-19 18:40:34 +0530

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -5,8 +5,8 @@ import json
 from main.plugins.helpers import TimeFormatter, humanbytes
 
 #------
-FINISHED_PROGRESS_STR = "🟢"
-UN_FINISHED_PROGRESS_STR = "🔴"
+FINISHED_PROGRESS_STR = "🖤"
+UN_FINISHED_PROGRESS_STR = "❤️‍🔥"
 DOWNLOAD_LOCATION = "/app"
 
 
