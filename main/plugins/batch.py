bb50ebfdf82cf88c0ec8af98a186c6691a3719c7
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2024-10-27 07:54:13 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("telethon").setLevel(logging.WARNING)
 
-MONGODB_CONNECTION_STRING = "mongodb+srv://crchoudharyji1122:Chenaram@2003@cluster0.cuje2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_CONNECTION_STRING = "mongodb+srv://chenakhichar112:Chenaram@2003@cluster0.shoit.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 OWNER_ID = 4573949804 # edit this
 LOG_GROUP = -1002374822952 # edit this
 
